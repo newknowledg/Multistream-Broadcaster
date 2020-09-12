390539d860ea8a3232745ac1ab218a023cdbed8c
king <sakyoung@protonmail.com>
2020-09-12 15:56:38 -0400

    modified:   msbroadcast/index.html 	modified:   msbroadcast/main.js

--- msbroadcast/main.js
+++ msbroadcast/main.js
@@ -2,12 +2,16 @@ const fBook = 'push rtmp://127.0.0.1:1936/rtmp/'
 const yTube = 'push rtmp://a.rtmp.youtube.com/live2/'
 const twitch = 'push '
 const peris = 'exec /usr/bin/ffmpeg -i rtmp://127.0.0.1/live/$name -crf 30 -preset ultrafast -acodec aac -strict experimental -ar 44100 -ac 2 -b:a 64k -vcodec libx264 -x264-params keyint=60:no-scenecut=1 -r 30 -b:v 500k -s 960x540 -f flv rtmp://ca.pscp.tv:80/x/'
- 
+const injButt = document.getElementById("inject")
+const rejButt = document.getElementById("reject")
+const addButt = document.getElementById("addS") 
+
 function emit(){
   var tskey = document.getElementsByClassName("s_key");
       tdest = document.getElementsByClassName("dest");
-      var skey = []
+  var skey = []
       fdest = []
+      
   for (var i = 0; i < tskey.length; i++){
          if (tdest[i].value == 'fb'){
           skey.push(fBook);}
@@ -23,6 +27,9 @@ function emit(){
          skey.push(tskey[i].value);
   }; 
    fetch('http://${receive}:4000/stream', { method: 'POST', headers:     {'content-type':'application/json'}, body: JSON.stringify(skey)})
+   injButt.disabled = true;
+   addButt.disabled = true;
+
 }
   function addLoc(){
           var temp = document.getElementById("tempd").content;
@@ -37,6 +44,8 @@ function emit(){
 function reneg(){
    var skey = []
    fetch('http://${receive}:4000/stream', { method: 'POST', headers:     {'content-type':'application/json'}, body: JSON.stringify(skey)})
+  injButt.disabled = false
+  addButt.disabled = false
       
 }
 
